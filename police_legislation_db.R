c01835f84f3f44ffab97fd41a118f58324306e2b
Eunice Kim <ekim89@smith.edu>
2021-08-02 16:55:49 -0400

    adding topic filter, and getting cleaned unique topics list

--- police_legislation_db.R
+++ police_legislation_db.R
@@ -42,6 +42,67 @@ policing_data <- policing_data %>%
 policing_data$State <- trimws(policing_data$State)
 policing_data$Local <- trimws(policing_data$Local)
 
+### Getting unique topics list ###
+topics_data <- as.data.frame(unique(unlist(strsplit(as.character(policing_data$Topic), ";")))) %>% 
+    rename("topic" = "unique(unlist(strsplit(as.character(policing_data$Topic), \";\")))") 
+# Renaming column to topic
+# Chose to make a separate topics_data data frame to just have a database of topics
+#and leave the original data the same
+
+topics_data$topic <- as.character(gsub('\\s$', "", topics_data$topic))
+topics_data$topic <- as.character(gsub('^\\s', "", topics_data$topic)) 
+# Removing trailing and leading white spaces
+
+topics_data <- as.data.frame(unique(topics_data$topic)) %>% 
+    rename("topic" = "unique(topics_data$topic)")
+# Renaming column to topic
+
+topics_data$topic[topics_data$topic == ""] <- NA
+#Removing "" observations
+
+### Cleaning unique topics list ###
+topics_data <- topics_data %>% 
+    mutate(topic = if_else(str_detect(topic, "(?i)bail"), "bail", topic),
+           topic = if_else(str_detect(topic, "(?i)arrest"), "arrest", topic),
+           topic = if_else(str_detect(topic, "(?i)body cam"), "body cameras", topic),
+           topic = if_else(str_detect(topic, "\\bcertification\\b"), "certification", topic),
+           topic = if_else(str_detect(topic, "(?i)civil liab"), "civil liability", topic),
+           topic = if_else(str_detect(topic, "(?i)criminal liab"), "criminal liability", topic),
+           topic = if_else(str_detect(topic, "(?i)data|(?i)data coll"), "data collection", topic),
+           topic = if_else(str_detect(topic, "^de\\w*ification$"), "decertification", topic),
+           topic = if_else(str_detect(topic, "biometric"), "biometric data", topic),
+           topic = if_else(str_detect(topic, "^dis\\w*ity$"), "disability", topic),
+           topic = if_else(str_detect(topic, "^disc\\w*ine$"), "discipline", topic),
+           topic = if_else(str_detect(topic, "disciplinary|discipinary"), "disciplinary record disclosure", topic),
+           topic = if_else(str_detect(topic, "duty to reprot"), "duty to report", topic),
+           topic = if_else(str_detect(topic, "firearm|fire arms"), "firearms", topic),
+           topic = if_else(str_detect(topic, "hiring"), "hiring", topic),
+           topic = if_else(str_detect(topic, "indigenous"), "indigenous affairs", topic),
+           topic = if_else(str_detect(topic, "prosecution of police|proseuction|prosecution"), "investigation/prosecution of police", topic),
+           topic = if_else(str_detect(topic, "knock"), "no knock warrants", topic),
+           topic = if_else(str_detect(topic, "oversight"), "oversight", topic),
+           topic = if_else(str_detect(topic, "^p\\w*nel"), "personnel management", topic),
+           topic = if_else(str_detect(topic, "ice in school"), "police in schools", topic),
+           topic = if_else(str_detect(topic, "protocal"), "protocol", topic),
+           topic = if_else(str_detect(topic, "immunity"), "qualified immunity", topic),
+           topic = if_else(str_detect(topic, "profil"), "racial profiling", topic),
+           topic = if_else(str_detect(topic, "sexual as"), "sexual assault", topic),
+           topic = if_else(str_detect(topic, "offender"), "sex offenders", topic),
+           topic = if_else(str_detect(topic, "standards"), "standards", topic),
+           topic = if_else(str_detect(topic, "study com"), "study commission", topic),
+           topic = if_else(str_detect(topic, "training"), "training", topic),
+           topic = if_else(str_detect(topic, "use of"), "use of force", topic),
+           topic = if_else(str_detect(topic, "search"), "search warrants", topic),
+           topic = if_else(str_detect(topic, "youth prog"), "youth programs", topic)) %>% 
+    distinct() %>% 
+    # Consolidating topics
+    na.omit() %>% 
+    arrange(topic)
+    # arrange in alphabetical order
+
+policing_data$Topic <- 
+    str_replace(policing_data$Topic, "\\bbody cam\\b", "body cameras")
+    
 ui <- fluidPage(
     setBackgroundColor(color = "LemonChiffon"),
     h2("Policing Legislation Registry"),
@@ -62,8 +123,14 @@ ui <- fluidPage(
                      
                      actionButton("reset_status", "Reset status filter"),
                      br(),
+                     br(),
                      
-                     ####fix slider after cleaning years
+                     uiOutput('resetable_topic'),
+                     
+                     actionButton("reset_topic", "Reset topic filter"),
+                     br(),
+                     br(),
+                     
                      sliderInput(
                          inputId = "year",
                          label = "Year",
@@ -76,9 +143,6 @@ ui <- fluidPage(
                      # actionButton('select', 'Select')
         ),
         mainPanel(
-            # fluidRow(
-            #     infoBoxOutput("rowcount")
-            # ),
             fluidRow(
                 column(width = 8,
                        box(dataTableOutput("policing_table"), width = NULL)
@@ -128,8 +192,17 @@ server <- function(input, output) {
         }
     })
     
+    filtered_topic <- reactive({
+        if(input$topic == "All"){
+            filtered_status()
+        } else {
+            filtered_status() %>% 
+                filter(str_detect(Topic, fixed(as.character(input$topic), ignore_case = TRUE)))
+        }
+    })
+    
     filtered_year <- reactive({
-        filtered_status() %>% 
+        filtered_topic() %>% 
             filter(Year >= input$year[1] & Year <= input$year[2])
     })
     
@@ -143,13 +216,6 @@ server <- function(input, output) {
             data = filtered_year(), rownames = FALSE
         )
     })
-    
-    # output$rowcount <- renderInfoBox({
-    #     infoBox(
-    #         "Number of total observations:",
-    #         as.character(nrow(policing_data))
-    #     )
-    # })
 
     output$resetable_state <- renderUI({
         times <- input$reset_state
@@ -184,13 +250,26 @@ server <- function(input, output) {
                 inputId = "status",
                 label = "Status (failed/enacted/pending):",
                 choices = c("All", unique(policing_data$Status)),
+                # There's an NA choice since some observations don't have a Status listed
                 #multiple = TRUE,
                 selected = "All"
             )
         )
     })
+    
+    output$resetable_topic <- renderUI({
+        times <- input$reste_topic
+        div(id = letters[(times %% length(letters))+1],
+            selectInput(
+                inputId = "topic",
+                label = "Topic of bill:",
+                choices = c("All", unique(topics_data$topic)),
+                selected = "All"
+            )
+        )
+    })
 }
-#how to clear all filters at once?
+#clearing all filters at once?
 #the local filter i have in mind: possible choices for local input change based on what state
 #is selected
 
